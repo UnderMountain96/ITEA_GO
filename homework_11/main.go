c7e0c6c0482dff481e7445922b551f0e059e1b79
UnderMountain96 <Bacss96@gmail.com>
2023-12-13 14:26:37 +0200

    fix comments

--- homework_11/main.go
+++ homework_11/main.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
-	outFiles := []string{"./input_1.txt", "./input_2.txt"}
+	inputFiles := []string{"./input_1.txt", "./input_2.txt"}
 	outFile := "./out.txt"
 	outLine := make(chan string)
 
 	var wg sync.WaitGroup
 
-	for _, p := range outFiles {
+	for _, p := range inputFiles {
 		wg.Add(1)
 		go func(path string) {
 			readFileLines(path, outLine)
@@ -28,18 +28,13 @@ func main() {
 		close(outLine)
 	}()
 
-	os.Remove(outFile)
-
-	for line := range outLine {
-		fmt.Println("write line: ", line)
-		writeFileLine(outFile, line)
-	}
+	writeFileLines(outFile, outLine)
 }
 
 func readFileLines(path string, outLine chan string) {
 	f, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
 	if err != nil {
-		log.Fatalf("open file error: %v", err)
+		log.Printf("open file error: %s\n", err)
 		return
 	}
 	defer f.Close()
@@ -50,15 +45,25 @@ func readFileLines(path string, outLine chan string) {
 	for fileScanner.Scan() {
 		outLine <- fileScanner.Text()
 	}
+
+	if err := fileScanner.Err(); err != nil {
+		log.Printf("scan file error: %s\n", err)
+		return
+	}
 }
 
-func writeFileLine(path string, line string) {
+func writeFileLines(path string, outLine chan string) {
+	os.Remove(path)
+
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
-		log.Fatalf("open file error: %v", err)
+		log.Printf("open file error: %s\n", err)
 		return
 	}
 	defer f.Close()
 
-	f.WriteString(line + "\n")
+	for line := range outLine {
+		fmt.Println("write line: ", line)
+		f.WriteString(line + "\n")
+	}
 }
